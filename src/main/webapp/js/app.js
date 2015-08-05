e90aacb5a3fe1077e91b7e0c7556bbceeaf2e279
Baptiste <morin.baptiste@gmail.com>
2015-08-06 00:14:57 +0200

    put libraries in project

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,17 +1,25 @@
-(function() {
+(function () {
     var app = angular.module('gemStore', []);
 
-    app.controller('StoreController', function() {
-        this.product = gem;
+    app.controller('StoreController', function () {
+        this.products = gems;
     });
 
-    var gem = {
-        name: 'Dodecahedron',
-        price: 110.50,
-        description: 'Some gems have hidden qualitites beyound their luster, beyond their shine... Dodeca is one of those gems.',
-        canPurchase: true,
-        soldOut: true
-    }
+    var gems = [
+        {
+            name: 'Dodecahedron',
+            price: 110.50,
+            description: 'Some gems have hidden qualitites beyound their luster, beyond their shine... Dodeca is one of those gems.',
+            canPurchase: true,
+            soldOut: false
+        },
+        {
+            name: "Pentagonal Gem",
+            price: 5.95,
+            description: '',
+            canPurchase: false
+        }
+    ];
 
 })();
 
