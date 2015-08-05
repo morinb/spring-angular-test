7ae23085a1fa29d57ec5f9654ed98d2a36834295
Baptiste <morin.baptiste@gmail.com>
2015-08-06 00:50:10 +0200

    move panel logic inside controller

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -7,6 +7,14 @@
 
     app.controller('PanelController', function(){
         this.tab = 1;
+
+        this.selectTab = function(setTab) {
+            this.tab = setTab;
+        }
+
+        this.isSelected = function(checkTab) {
+            return this.tab === checkTab;
+        }
     });
 
     var gems = [{
